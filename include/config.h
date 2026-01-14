4ba9e05ef029f9b261ea126f32291a65be8f087e
Sebastian Schmidt <sebastian.schmidt@my.mail.de>
2026-01-14 08:32:09 +0100

    Update temperature sensor update interval to 60 seconds to prevent button blockade

--- include/config.h
+++ include/config.h
@@ -34,7 +34,7 @@
 
 // Temperature Sensor (DS18B20)
 #define TEMP_SENSOR_PIN 33      // GPIO 2 (Fix für falsche Verdrahtung)
-#define TEMP_UPDATE_INTERVAL_MS 120000 // Measure every 120s (Verhindert Button-Blockade)
+#define TEMP_UPDATE_INTERVAL_MS 60000 // Measure every 60s (Verhindert Button-Blockade)
 
 // IMU Configuration (BNO085)
 #define IMU_SDA 21
